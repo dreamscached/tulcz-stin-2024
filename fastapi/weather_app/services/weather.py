3f6f63dcac2bd48d48ee8a9d912f939b70187369
dreamscached <dreamscache.d@gmail.com>
2024-05-20 22:52:09 +0200

    feat: extend WeatherService with new method and add new dataclasses

--- fastapi/weather_app/services/weather.py
+++ fastapi/weather_app/services/weather.py
@@ -4,26 +4,77 @@ historical data, geographic name to coordinates mapping, etc.
 """
 
 from abc import ABC, abstractmethod
-from typing import Tuple, List
+from dataclasses import dataclass
+from enum import Enum
+import datetime
 import os
 
 from weather_app.services.cache import CacheService, get_cache_service
 from weather_app.services.thread import ThreadPoolService, get_thread_pool_service
 
 from pyowm import OWM
+from pyowm.weatherapi25.weather import Weather
 from pyowm.weatherapi25.location import Location
 from fastapi import Depends
 
-GeoCoord = Tuple[float, float]
+@dataclass
+class ToponymData:
+    """Common data class for toponym name and coordinate.s"""
+    toponym: str
+    country: str
+    latitude: float
+    longitude: float
+
+class WeatherType(Enum):
+    """Enum type for various weather types."""
+    RAIN = "Rain"
+    CLEAR = "Sun"
+    FOG = "Fog"
+    CLOUDY = "Clouds"
+    SNOW = "Snow"
+    STORM = "Storm"
+    TORNADO = "Tornado"
+    HURRICANE = "Hurricane"
+
+@dataclass
+class Temperature:
+    """Data class for temperature that includes extra data."""
+    temperature: float
+    min_temperature: float
+    max_temperature: float
+    feels_like: float
+
+@dataclass
+class Forecast:
+    """Data class for a forecast details object."""
+    reference_time: datetime.datetime
+    weather_type: WeatherType
+    temperature: Temperature
+
+class TempUnit(Enum):
+    """Enum type for temperature units."""
+    KELVIN = "kelvin"
+    CELSIUS = "celsius"
+    FAHRENHEIT = "fahrenheit"
 
 class WeatherService(ABC):
     """Abstract class for weather service implementations to extend."""
 
     @abstractmethod
-    async def get_toponym_coordinates(self, toponym: str) -> List[GeoCoord]:
+    async def get_query_toponym(self, toponym: str) -> list[ToponymData]:
         """
         Performs a lookup to retrieve geographical coordinates for a given
         toponym string and returns a list of possible matches.
+        """
+    @abstractmethod
+    async def get_weather_forecast(
+        self,
+        location: tuple[float, float],
+        temp_type: TempUnit = TempUnit.CELSIUS
+    ) -> list[Forecast]:
+        """
+        Performs a forecast fetch for the specified location and returns
+        a list of weather forecast details.
         """
 
 
@@ -51,9 +102,31 @@ class OpenWeatherService(WeatherService):
         self._cache = cache
         self._th_pool = thread_pool
 
-    async def get_toponym_coordinates(self, toponym: str) -> List[GeoCoord]:
-        def to_geo_coord(it: Location) -> GeoCoord:
-            return (it.lat, it.lon)
+    async def get_query_toponym(self, toponym: str) -> list[ToponymData]:
+        def to_toponym(it: Location) -> ToponymData:
+            return ToponymData(it.name, it.country, it.lat, it.lon)
 
         raw = await self._th_pool.run_in_thread(self._geocode.geocode, toponym)
-        return list(map(to_geo_coord, raw))
+        return list(map(to_toponym, raw))
+
+    async def get_weather_forecast(
+        self,
+        location: tuple[float, float],
+        temp_type: TempUnit = TempUnit.CELSIUS
+    ) -> list[Forecast]:
+        forecast_raw = await self._th_pool.run_in_thread(
+            self._weather.forecast_at_coords,
+            *location, "3h")
+
+        def to_forecast(weather: Weather) -> Forecast:
+            temperature_raw = weather.temperature(temp_type.value)
+            temperature = Temperature(temperature_raw["temp"],
+                                      temperature_raw["temp_min"],
+                                      temperature_raw["temp_max"],
+                                      temperature_raw["feels_like"])
+
+            return Forecast(weather.ref_time,
+                            WeatherType(weather.status),
+                            temperature)
+
+        return list(map(to_forecast, forecast_raw.forecast.weathers))
